43e427f2510d0f32f1c8c7ad87bf24681b93d861
FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>
2026-05-30 12:45:44 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from astrbot.api.event import filter, AstrMessageEvent
+from astrbot.api.star import Context, Star, register
+from astrbot.api import logger
+from astrbot.core.config.astrbot_config import AstrBotConfig
+import httpx
+import json
+
+
+@register("http_bridge", "FlyModeZ", "", "0.1.0")
+class HttpBridge(Star):
+    def __init__(self, context: Context, config: AstrBotConfig):
+        super().__init__(context)
+        
+        self.http_endpoint = config.get("http_endpoint")
+
+    async def initialize(self):
+        self.client = httpx.AsyncClient(http2=True, timeout=10.0)
+        logger.info("HTTP 客户端已初始化。")
+
+    async def terminate(self):
+        if self.client:
+            await self.client.aclose()
+            logger.info("HTTP 客户端已关闭。")
+
+    # @filter.event_message_type(filter.EventMessageType.ALL, priority=10)
+    @filter.command("fm")
+    async def ask_service(self, event: AstrMessageEvent, content: str):
+        payload = {
+            "user_id": event.unified_msg_origin,
+            "content": content
+        }
+
+        async with self.client.stream("POST", self.http_endpoint, json=payload) as response:
+            response.raise_for_status()
+            async for line in response.aiter_lines():
+                if line:
+                    data = json.loads(line)
+                    yield event.plain_result(data.get("text"))
